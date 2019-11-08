3d9d17861eb61ef837601f0230870065c05ac650
Oleg Kosin <olegkosin@rambler.ru>
2019-11-08 23:16:01 +0300

    + simple codeLoader

--- src/main/java/CodeLoader.java
+++ src/main/java/CodeLoader.java
@@ -1,24 +1,45 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Loads text of the program
  */
 public class CodeLoader {
 
-    private String text;
+    //private String text;
+    private String curPath;
+    private List<String> listOfCode = Collections.emptyList();
 
-    CodeLoader (String path){
+    CodeLoader(String path) {
+        this.curPath = path;
+        listOfCode = LoadCodeToList();
+        listOfCode = CleanListOfCode(listOfCode);
+    }
+
+    private List<String> CleanListOfCode(List<String> listOfCode) {
+        for (int i = 0; i < listOfCode.size(); i++) {
+            listOfCode.set(i, listOfCode.get(i).trim());
+        }
+        return listOfCode;
+    }
+
+    private List<String> LoadCodeToList() {
+        List<String> lines = Collections.emptyList();
         try {
-            this.text = new String(Files.readAllBytes(Paths.get(path)));
+            lines = Files.readAllLines(Paths.get(curPath), StandardCharsets.UTF_8);
         } catch (IOException e) {
-            System.out.println("Read file error");
             e.printStackTrace();
         }
+        return lines;
     }
 
-    public String getText() {
-        return text;
+    public List<String> getListOfCode() {
+        return listOfCode;
     }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main (String args[]){
 
+    CodeLoader mainCodeLoader = new CodeLoader("src\\main\\resources\\ProgrammText1.prog");
+
 
     }
 }
